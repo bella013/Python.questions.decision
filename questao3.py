6e5013f7c0f6652c97882f864faf5132f20e74c8
Izabella Jorge <elyeiza@hotmail.com>
2020-08-24 23:51:32 -0300

    Questão 3 modificada

---
+++ questao3.py
@@ -0,0 +1,7 @@
+letra = str(input("Insira a letra referente ao seu sexo: "))
+if(letra=="F" or letra=="f"):
+    print("Feminino")
+elif(letra=="M" or letra=="m"):
+    print("Masculino")
+else:
+    print("Sexo inválido")
