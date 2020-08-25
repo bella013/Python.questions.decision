ba1e6ac9cf56023717e4855574ff250d7e4e0786
Izabella Jorge <elyeiza@hotmail.com>
2020-08-25 00:05:22 -0300

    Questão 4 adicionadah moreh

---
+++ questao4.py
@@ -0,0 +1,7 @@
+letra = str(input("Insira uma LETRA: "))
+if(letra=="A" or letra=="E" or letra=="I" or letra=="O" or letra=="U"):
+    print("Vogal")
+elif (letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u"):
+    print("vogal")
+else:
+    print("Consoante")
