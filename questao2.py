0ebc2519e891440de865994675934fb4af1fc62e
Izabella Jorge <elyeiza@hotmail.com>
2020-08-24 23:35:33 -0300

    Nova questão adicionada

---
+++ questao2.py
@@ -0,0 +1,7 @@
+valor = float(input("Insira um valor numérico: "))
+if(valor>0):
+    print("O número ",valor ," é positivo")
+elif (valor<0):
+    print("O número ",valor ," é negativo")
+else:
+    print("O número é neutro") #caso o número seja 0
