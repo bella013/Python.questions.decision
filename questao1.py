721ce4c5cf14a80bf8293f4069d3fe9337ecfd97
Izabella Jorge <elyeiza@hotmail.com>
2020-08-24 23:27:03 -0300

    Questão 1 modificada

---
+++ questao1.py
@@ -0,0 +1,9 @@
+num1 = int(input("Informe um número inteiro: "))
+num2 = int(input("Informe outro inteiro: "))
+
+if(num1>num2):
+    print("O maior número é:", num1)
+elif (num2>num1):
+    print("O maior número é: ", num2)
+else:
+    print("Os números tem o mesmo valor")
